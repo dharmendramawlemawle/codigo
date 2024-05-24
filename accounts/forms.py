b6d9ea3251b6e292b5f1720c6d5b43d40b71f1f7
dharmendramawlemawle <69690659+dharmendramawlemawle@users.noreply.github.com>
2024-05-24 21:49:00 +0530

    Add files via upload
    create accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+# accounts/forms.py
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Username', max_length=255)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
